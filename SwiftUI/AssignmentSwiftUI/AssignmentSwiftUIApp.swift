9e6a64166b7c389bc9275139ca2d366ecc4b517a
Quan <urjhams@gmail.com>
2020-11-09 23:42:02 +0100

    init

---
+++ SwiftUI/AssignmentSwiftUI/AssignmentSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentSwiftUIApp.swift
+//  AssignmentSwiftUI
+//
+//  Created by Quân Đinh on 09.11.20.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/AssignmentSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AssignmentSwiftUI
+//
+//  Created by Quân Đinh on 09.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
