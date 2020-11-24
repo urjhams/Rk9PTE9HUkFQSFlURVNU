e0ca63bb47b69403ef07d4ddea43a0300ce458b1
Quan <urjhams@gmail.com>
2020-11-24 16:51:04 +0100

    Update AssignmentSwiftUIApp.swift
    dissmiss keyboard behaviour

--- SwiftUI/AssignmentSwiftUI/AssignmentSwiftUIApp.swift
+++ SwiftUI/AssignmentSwiftUI/AssignmentSwiftUIApp.swift
@@ -12,6 +12,28 @@ struct AssignmentSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
             CityListView()
+                .onAppear(perform: UIApplication.shared.addTapGestureRecognizer)
         }
     }
 }
+
+extension UIApplication: UIGestureRecognizerDelegate {
+    
+    public func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
+        return true
+    }
+    
+    func addTapGestureRecognizer() {
+        guard let window = windows.first else { return }
+        let tapGesture = UITapGestureRecognizer(target: window,
+                                                action: #selector(UIView.endEditing))
+        tapGesture.requiresExclusiveTouchType = false
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.delegate = self
+        window.addGestureRecognizer(tapGesture)
+    }
+}
+
